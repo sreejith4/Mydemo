0187bbef302f9588c7e76bae20f3f7eb96f31616
sreejith4edureka <sreejith4edureka@gmail.com>
2021-01-08 08:28:37 +0000

    New Java class for Demo2

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,6 @@
 class Demo2{
 	public static void main(String args[]){
-		System.out.println("This is a demo java class");
+		System.out.println("This is a demo java class demo2");
+		System.out.println("This is a 2nd line from  java class demo2");
 	}
 }
---
+++ Demo3.java
@@ -0,0 +1,6 @@
+class Demo2{
+	public static void main(String args[]){
+		System.out.println("This is a demo java class demo3");
+		System.out.println("This is a 2nd line from  java class demo3");
+	}
+}
